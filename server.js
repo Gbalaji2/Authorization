02331a8435ba0a95569f6b3972551af58d992bed
Gowthambalaji <sbalajigowtham@gmail.com>
2025-09-27 20:10:52 +0530

    main

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use('/api/auth', authRoutes);
 
 app.use((req, res) => res.status(404).json({ message: 'Route not found' }));
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
